fc1b8f3e3870635b262bdfd4ab506ed01097ac4b
austin <austinpearce15@yahoo.com>
2020-05-26 18:51:47 -0500

    Continue work on errors with apostrophes with Spotify's API.

--- RipperProject/RipperApp/find_album.py
+++ RipperProject/RipperApp/find_album.py
@@ -8,15 +8,14 @@ def get_album(artist_name, title):
     client_credentials_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
     sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
-    # Remove apostrophes if applicable
-    artist_name = artist_name.replace("'", "")
-    title = title.replace("'", "")
-
     results = sp.search(q='artist:' + artist_name + ' track:' + title, type='track')
 
     # Check if found in Spotify's database
     if len(results['tracks']['items']) == 0:
-        return "Invalid", "Invalid"
+        # Try title without apostrophes
+        results = sp.search(q='artist:' + artist_name + ' track:' + title.replace("'", ""), type='track')
+        if len(results['tracks']['items']) == 0:
+            return "Invalid", "Invalid"
 
     cover = results['tracks']['items'][0]['album']['images'][0]['url']
     album_name = results['tracks']['items'][0]['album']['name']
